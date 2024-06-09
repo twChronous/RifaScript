44758d36d626b2a5b3f7b6b551206888822656d4
luid <eng.limaluis@gmail.com>
2024-06-09 18:34:42 -0300

    dinamicamente criando a tabela

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() { 
+  
+  let vendedores = [
+    { nome: "Luis Eduardo Lima", rifas: 0 },
+    { nome: "a", rifas: 0 },
+    { nome: "b", rifas: 0 },
+    { nome: "c", rifas: 0 }
+  ];
 
-document.addEventListener("DOMContentLoaded", () => { 
-  const hideButton = document.getElementById("hideButton");
-  const vendedoresTable = document.getElementById("VendedoresTable");
+  let divrfTable = document.getElementById("rifasTable");
+  let rftable = document.createElement("table");
+  let rftableHead = rftable.createTHead();
+  let linerftableHead =rftable.insertRow();
+  linerftableHead.insertCell().textContent = "Vendedor";
+  linerftableHead.insertCell().textContent = "Rifas Vendidas";
+  let rftableBody = rftable.createTBody(); 
 
+  vendedores.forEach(vendedor => {
+    let line = rftableBody.insertRow();
+    line.insertCell().textContent = vendedor.nome;
+    line.insertCell().textContent = vendedor.rifas;
+  });
+  
+  divrfTable.appendChild(rftable);
+  
+  let hideButton = document.getElementById("tableButton");
   hideButton.addEventListener("click", () => { 
-    if (vendedoresTable.style.display === "none") {
-      vendedoresTable.style.display = "block";
+    if (divrfTable.style.display === "none") {
+      divrfTable.style.display = "block";
+      hideButton.textContent = "Esconder Tabela";
     } else {
-      vendedoresTable.style.display = "none";
+      divrfTable.style.display = "none";
+      hideButton.textContent = "Mostrar Tabela";
     }
   });
 });
+
+
 
