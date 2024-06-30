cb9bf8b3828fb3feb1aa9fe5d9244bf6f74c3958
Chronous <lucasmateustds@gmail.com>
2024-06-30 18:28:42 -0300

    :lipstick: feat: Estilizaçao botão salvar corrigida

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from "@/_utils/types";
 export default function Button(props: ButtonProps) {
     return (
         <div className="mt-5">
-            <button className="bg-green-500 p-5 rounded w-60" onClick={props.onClick}>
+            <button className="bg-green-500 text-white p-5 rounded w-60" onClick={props.onClick}>
                 {props.text}
             </button>
         </div>
